7847396f5262320e2a5bafb68e40ceb071e7b9fd
e <minnki19@gmail.com>
2024-04-24 14:45:50 +0900

    create done

--- src/main/kotlin/com/daelim/springtest/main/controller/UserController.kt
+++ src/main/kotlin/com/daelim/springtest/main/controller/UserController.kt
@@ -1,4 +1,25 @@
 package com.daelim.springtest.main.controller
 
-class UserController {
+import com.daelim.springtest.main.api.model.dto.UserDto
+import com.daelim.springtest.main.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/user")
+class UserController(private val userService: UserService) {
+
+    // ...
+
+    @PostMapping("/create")
+    fun createUser(@RequestBody userDto: UserDto): ResponseEntity<String> {
+        // 회원가입 처리 로직 호출
+        val isUserCreated = userService.createUser(userDto)
+        return if (isUserCreated) {
+            ResponseEntity.ok("회원가입 성공")
+        } else {
+            ResponseEntity("이미 존재하는 이메일입니다.", HttpStatus.BAD_REQUEST)
+        }
+    }
 }
--- src/main/kotlin/com/daelim/springtest/main/service/UserService.kt
+++ src/main/kotlin/com/daelim/springtest/main/service/UserService.kt
@@ -1,4 +1,23 @@
 package com.daelim.springtest.main.service
 
-class UserService {
+
+import com.daelim.springtest.main.api.model.dto.UserDto
+import com.daelim.springtest.main.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(private val userRepository: UserRepository) {
+
+    // ...
+
+    fun createUser(userDto: UserDto): Boolean {
+        // 이메일 중복 체크
+        val existingUser = userRepository.findByEmail(userDto.email)
+        if (existingUser != null) {
+            return false
+        }
+        // 사용자 정보 저장
+        userRepository.save(userDto)
+        return true
+    }
 }
