c0cce1b91956535610f9d02fc981a62b89dda6bd
e <minnki19@gmail.com>
2024-04-24 14:17:09 +0900

    first commit

--- src/main/kotlin/com/daelim/springtest/SpringTestApplication.kt
+++ src/main/kotlin/com/daelim/springtest/SpringTestApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class SpringTestApplication
 
+//go
 fun main(args: Array<String>) {
 	runApplication<SpringTestApplication>(*args)
 }
